4bc571ae5f36efb01ac8d262d7b891c51f31b3ee
abhi-swami <abhi-swami@gmail.com>
2026-03-08 17:04:56 +0530

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // 👈 important for Cloudflare Pages
   reactStrictMode: true,
+
   images: {
+    unoptimized: true, // 👈 required for static export
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '**github-readme-stats.vercel.app',
-        port: '',
-        pathname: '/*',
+        protocol: "https",
+        hostname: "**github-readme-stats.vercel.app",
+        pathname: "/*",
       },
       {
-        protocol: 'https',
-        hostname: '**github-readme-streak-stats.herokuapp.com',
-        port: '',
-        pathname: '/*',
-        search: '?*',
+        protocol: "https",
+        hostname: "**github-readme-streak-stats.herokuapp.com",
+        pathname: "/*",
+        search: "?*",
       },
       {
-        protocol: 'https',
-        hostname: 'github-readme-activity-graph.vercel.app',
-        port: '',
-        pathname: '/**',
-      }
+        protocol: "https",
+        hostname: "github-readme-activity-graph.vercel.app",
+        pathname: "/**",
+      },
     ],
   },
 };
